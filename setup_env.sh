828029854b1516a1b2bb1ed1a8311f02730a307f
Kapil Parab <kapilparab@arizona.edu>
2026-04-24 14:42:42 -0700

    Comment fix

--- setup_env.sh
+++ setup_env.sh
@@ -27,8 +27,6 @@ pip install deepspeed==0.13.1
 
 pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.6/flash_attn-2.5.6+cu118torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
-echo "Starting to merge LoRA weights with the base model..."
-
 # cd ..
 
 # unzip -q checkpoint-800.zip -d GeoPixel/
